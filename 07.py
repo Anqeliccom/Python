7ca7030bfd1c24cc47d35ad6ee52e1a3faf53eb1
Anqeliccom <hamidulinaanzelika462@gmail.com>
2023-10-23 01:16:47 +0700

    Added new version pf 07.py

--- 07.py
+++ 07.py
@@ -1,23 +1,15 @@
-
 list1 = [1, 2, [4, 5], [6, [7]], 8]
-newList = []
 
 def flatten(lst, depth=None, current_depth=0):
+    newList = []
     for x in lst:
         if isinstance(x, list) and (depth is None or current_depth < depth):
-            flatten(x, depth, current_depth + 1)
+            newList.extend(flatten(x, depth, current_depth + 1))
         else:
             newList.append(x)
+    return newList
 
-flatten(list1, depth=1)
-print(newList)
-
-
-
-
-
-
-
-
+flattened_list = flatten(list1, depth=1)
+print(flattened_list)
 
 
